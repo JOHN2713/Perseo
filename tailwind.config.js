440cb374d140121edc386b7dd931b4d9a7258b52
Johny Vilaña <johnyv1305@gmail.com>
2025-07-21 20:46:07 -0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+extend: {
+  animation: {
+    wave: 'wave 5s linear infinite'
+  }
+}
+// tailwind.config.js
+safelist: [
+  'hidden',
+  'block',
+  'hover:scale-105',
+  'drop-shadow-xl',
+  'transition'
+]
